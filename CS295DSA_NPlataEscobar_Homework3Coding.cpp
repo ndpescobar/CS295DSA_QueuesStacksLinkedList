ffa8352d9cc12ef6a4b18df90cf22a5c4f3f4a5d
Nicolas <ndpescobar@gmail.com>
2019-06-20 23:52:51 -0700

    Update CS295DSA_NPlataEscobar_Homework3Coding.cpp

--- CS295DSA_NPlataEscobar_Homework3Coding.cpp
+++ CS295DSA_NPlataEscobar_Homework3Coding.cpp
@@ -1,24 +1,24 @@
 /*	Homework 3 Coding - Queues, Stacks, and Link Lists.
 	@author: Nicolas Plata-Escobar.
-	@version: v0.1, 06/19/2019.
+	@version: v0.6, 06/20/2019.
 */
 #include <iostream>
 #include <fstream>
+#include <array>
 #include <string>
-#include <stack>
-#include <queue>
-#include <list>
-#include "Game Database/GameData.cpp"
+//#include <stack>
+//#include <queue>
+//#include <list>
+//#include "Game Database/GameData.cpp"
 using namespace std;
 
 class Stack {
-	int MAX = 1024;
+	const int MAX = 1024;
 	string *arrS;
 	int top;
-	
 public:
-	string arrS[MAX];
 	Stack(int size) {
+		arrS = new int[size];
 		top = -1;
 	}
 
@@ -44,10 +44,17 @@ public:
 		}
 	}
 
-	string FindDeveloper(string developer) {
-		
+	//traverse through the stack's data for developer to return a bool
+	bool FindDeveloper(string developer) {
+		while (top >= 0) {
+			if (pop() == developer) { //may need to parse string for GameData values/categories
+				return true;
+			}
+		}
+		return false;
 	}
 
+	//adds a string to the top of the stack
 	void AddToEnd(string gameTitle) {
 		// arrays are not dynamic.
 		if (top >= (MAX - 1)) {
@@ -59,6 +66,7 @@ public:
 		}
 	}
 
+	//write out contents of the stack to a CSV file, fileName
 	void WriteCSV(string fileName) {
 		ofstream fileCSV;
 		cout << "Opening" << fileName << endl;
@@ -75,66 +83,162 @@ public:
 };
 
 class Queue {
+	string *arrQ;
+	int maxSize;
+	string front;
+	string back;
+	int currentSize;
 public:
-	string a[MAX];
-	Stack() { top = -1; }
-
+	
+	Queue(int size) {
+		arrQ = new int[size];
+		maxSize = size;
+		front = 0;
+		back = -1;
+		currentSize = 0;
+	}
+	
+	//returns current size of the queue
+	int size(){
+		return currentSize;
+	}
+	
+	//check if queue is full
+	bool isFull(){
+		return (size()==maxSize);
+	}
+	
+	//check if queue is empty
+	bool isEmpty(){
+		return (size()==0);
+	}
+	
+	//adds an element to the back of the queue
 	bool Enqueue(string s) {
-		
+		if(isFull()){
+			cout << "Queue Overflow" << endl;
+			return false;
+		}
+		else {
+			back = size() - 1;
+			arrQ[back] = s;
+			currentSize++;
+			return true;
+		}
 	}
 
+	//removes an element from the front of the queue
 	string Dequeue() {
-		
+		if (isEmpty()) {
+			cout << "Queue Underflow" << endl;
+			return "";
+		}
+		else {
+			string oldString = arrQ[front];
+			front = size() - (size() - 1);
+			currentSize++;
+			return oldString;
+		}
 	}
 
-	string FindDeveloper(string developer) {
-
+	//traverse through the queue's data for developer to return a bool
+	bool FindDeveloper(string developer) {
+		for (int i = back; i > 0; i--) {
+			if (arrQ[i] == developer) { //may need to parse string for GameData values/categories
+				return true;
+			}
+		}
+		return false;
 	}
 
-	void AddToEnd(string gameTitle) {
-		
+	void AddToEnd(string gameTitle) { //same as enqueue?
+		if(isFull()){
+			cout << "Queue Overflow; queue is full" << endl;
+			return false;
+		}
+		else {
+			back = size() - 1;
+			arrQ[back] = s;
+			currentSize++;
+			return true;
+		}
 	}
 };
 
 struct Node {
 	string value;
 	struct Node* next;
-	struct Node* next;
+	struct Node* prev;
 	struct Node* head
 	
-	struct Node* GetNewNode(string gameTitle){
+	struct Node* CreateNewNode(string gameTitle){
 		struct Node* newNode = malloc(sizeof(struct Node));
-		newNode->value
+		newNode->value = gameTitle;
+		newNode->next = NULL;
+		newNode->prev = NULL;
+		return newNode;
 	}
 	
+	//create new node at end of the list with gameTitle
 	void InsertEnd(string gameTitle){
-		//create new node at end of the list with gameTitle
+		struct Node* newNode = CreateNewNode(gameTitle);
+		struct Node* traverse = head;
+		while (traverse != NULL) {
+			traverse = traverse->next;
+		}
+		if (traverse == NULL) {
+			traverse->next = newNode;
+		}
+		else {
+			cout << "error: failed to insert new node at end" << endl;
+		}
 	}
 	
 	//create new node at beginning/head of the list with gameTitle
 	void InsertBegin(string gameTitle){
-		struct Node* 
+		struct Node* newNode = CreateNewNode(gameTitle);
+		if (head == NULL) {
+			head = newNode;
+		}
+		head->prev = newNode;
+		newNode->next = head;
+		head = newNode;
 	}
 	
+	//traverse to node with insertionTitle, and create a new node with gameTitle after insertionTitle
 	void InsertAfter(string gameTitle, string insertionTitle){
-		//traverse to node with insertionTitle, and create a new node with gameTitle after insertionTitle
+		struct Node* newNode = CreateNewNode(gameTitle);
+		struct Node* traverse = head;
+		while (traverse->data != insertionTitle) {
+			traverse = traverse->next;
+		}
+		if (traverse->data == insertionTitle) {
+			//possibly incorrect linking 
+			traverse->prev = newNode;
+			newNode->next = traverse;
+			traverse = newNode;
+		}
+		else {
+			cout << "error: failed to insert new node after " << insertionTitle << endl;
+		}
 	}
 	
+	
+	//traverse list for gameTitle, and return true if found
 	bool Delete(string gameTitle){
-		//traverse list for gameTitle, and return true if found
+		
 	}
 	
+	//traverse list for developer, and return the line/node
 	string FindGame(string developer){
-		//traverse list for developer, and return the line/node
+		
 	}
 };
 
 int main() {
 	//Stack - compute total number of arrayIndexes (data[i] operations) for all operations.
-	stack <GameData> stackGD;
 
 	//Queue - compute total number of arrayIndexes (data[i] operations) for all operations.
-	queue <GameData> queueGD;
 
 	//Doubly Linked List (prevNode and nextNode) - compute total number of pointer traversals.
 
